ccd60ec90f50dd72f33f93066c9fb69673885fe2
0Yasser <yasiirh@gmail.com>
2022-03-07 14:05:15 +0300

    handling multiple arguments(args) and keyword arguments(kwargs)

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ error_message1 = 'VALUE MUST BE OF TYPE STRING';
 error_message2 = 'STRING MUST BE LONGER THAN FIVE LETTERS'
 
 def myDecorator(params):
-    def wrapper(arg):
-        if type(arg) is not str:
-            raise TypeError(error_message1)
-        if len(arg)<6:
-            raise ValueError(error_message2)
-        return params(arg)
+    def wrapper(*args,**kwargs):
+        for arg in args:
+            print('arg',arg)
+            if type(arg) is not str:
+                raise TypeError(error_message1)
+            if len(arg)<6:
+                raise ValueError('aaa'+error_message2)
+        for kwarg in kwargs:
+            if type(kwargs[kwarg]) is not str:
+                raise TypeError(error_message1)
+            if len(kwargs[kwarg])<6:
+                raise ValueError(error_message2)
+        return params(*args,**kwargs)
     return wrapper
 
 @myDecorator
-def main_func(param):
-    print('Param:',param)
+def main_func(param1,param2):
+    print('Param:',param1,param2)
     
-main_func('python is fu')
-main_func('Hi')
-main_func(1)
+main_func('python is fu',param2='n, ...')
